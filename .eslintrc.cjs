043272c934fb191765d1af94211ee6f532e06087
Douglas <mendes-barreto@Live.com>
2023-01-07 16:08:27 -0500

    chore: Upate configs to allow module alias on eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,44 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
+    env: {
+        node: true,
+    },
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'import'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:import/typescript',
+        'plugin:import/warnings',
+        'plugin:import/errors',
         'airbnb-base',
         'prettier',
+        'plugin:prettier/recommended',
     ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        project: './tsconfig.json',
+    settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+            node: {
+                paths: [
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'node_modules'),
+                ],
+            },
+        },
     },
     rules: {
         indent: 'off',
