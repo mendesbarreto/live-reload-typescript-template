19a03199bc6b3c5cc4f8b69cdfba7fb1b2dbff08
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2025-09-17 12:04:16 -0400

    chore: Remove eslint from project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { DateTime } from 'luxon';
+
+const updatedStartedAt: string | Date = new Date();
+
+const updatedStartedAtDate =
+  typeof updatedStartedAt === 'string'
+    ? updatedStartedAt
+    : updatedStartedAt.toISOString();
+
+console.log(updatedStartedAtDate);
+const currentMonitoringDate =
+  DateTime.fromISO(updatedStartedAtDate).toFormat('yyyy-MM-dd');
+
+console.log(currentMonitoringDate);
